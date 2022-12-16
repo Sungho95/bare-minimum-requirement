20f19d7edcbf919076655121c00d89bafc6c2348
Sungho95 <tjdgh1785@gmail.com>
2022-12-16 11:27:58 +0900

    initial

---
+++ src/main/java/com/codestates/bareminimumrequirement/BareMinimumRequirementApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.bareminimumrequirement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BareMinimumRequirementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BareMinimumRequirementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/bareminimumrequirement/BareMinimumRequirementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.bareminimumrequirement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BareMinimumRequirementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
